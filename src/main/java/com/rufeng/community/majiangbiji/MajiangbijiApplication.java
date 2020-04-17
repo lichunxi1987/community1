a18b9322da62fa54eccaf4bad0bf20e52837390e
skylee <349018588@qq.com>
2020-04-17 09:01:18 +0800

    init repository

---
+++ src/main/java/com/rufeng/community/majiangbiji/MajiangbijiApplication.java
@@ -0,0 +1,13 @@
+package com.rufeng.community.majiangbiji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajiangbijiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MajiangbijiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rufeng/community/majiangbiji/MajiangbijiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rufeng.community.majiangbiji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajiangbijiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
